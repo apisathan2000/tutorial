aeca5e3ab42739521bbf979c5302cbc51b1dcb6f
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2025-04-28 09:19:45 +0530

    events introduction

--- src/index.js
+++ src/index.js
@@ -32,7 +32,45 @@ function BookList() {
 }
 
 function EventExamples() {
-  return <h1>Events</h1>;
+  const handleButtonClick = function () {
+    alert("Handle Button Click !");
+  };
+
+  const handleFormInput = function (e) {
+    console.log(e);
+    console.log(e.target.name);
+    console.log(e.target.value);
+    console.log("Handle Form Input !");
+  };
+
+  const handleFormSubmission = function (e) {
+    e.preventDefault();
+    console.log("From Submitted !");
+    // alert("Handle form submission !");
+  };
+
+  return (
+    <section>
+      <form action="">
+        {/* <form action="" onSubmit={handleFormSubmission}> */}
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          id=""
+          style={{ margin: "1rem 0" }}
+          onChange={handleFormInput}
+        />
+        <div>
+          <button type="submit" onClick={handleFormSubmission}>
+            Submit Form
+          </button> 
+        </div>
+      </form>
+
+      <button onClick={handleButtonClick}>Click Me</button>
+    </section>
+  );
 }
 
 function Book(props) {

43bd50b29151fbd4523c38fb27b41d463f4d3519
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2025-04-30 05:53:14 +0530

    props example

--- src/index.js
+++ src/index.js
@@ -20,66 +20,104 @@ const bookList = [
 ];
 
 function BookList() {
+  const someValue = "shakeAndBake";
+  const displayValue = function () {
+    console.log(someValue);
+  };
+
+  const getBook = function (id) {
+    const book = bookList.find(function (book) {
+      return book.id === id;
+    });
+
+    console.log(id);
+  };
+
   return (
     <section className="booklist">
-      <EventExamples />
+      {/* <EventExamples /> */}
 
       {bookList.map(function (book) {
-        return <Book book={book} key={book.id} />;
+        return (
+          <Book
+            book={book}
+            key={book.id}
+            displayValue={displayValue}
+            getBook={getBook}
+          />
+        );
       })}
     </section>
   );
 }
 
-function EventExamples() {
-  const handleButtonClick = function () {
-    alert("Handle Button Click !");
-  };
+// function EventExamples() {
+//   const handleButtonClick = function () {
+//     alert("Handle Button Click !");
+//   };
 
-  const handleFormInput = function (e) {
-    console.log(e);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    console.log("Handle Form Input !");
-  };
+//   const handleFormInput = function (e) {
+//     console.log(e);
+//     console.log(e.target.name);
+//     console.log(e.target.value);
+//     console.log("Handle Form Input !");
+//   };
 
-  const handleFormSubmission = function (e) {
-    e.preventDefault();
-    console.log("From Submitted !");
-    // alert("Handle form submission !");
-  };
+//   const handleFormSubmission = function (e) {
+//     e.preventDefault();
+//     console.log("From Submitted !");
+//     // alert("Handle form submission !");
+//   };
 
-  return (
-    <section>
-      <form action="">
-        {/* <form action="" onSubmit={handleFormSubmission}> */}
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          id=""
-          style={{ margin: "1rem 0" }}
-          onChange={handleFormInput}
-        />
-        <div>
-          <button type="submit" onClick={handleFormSubmission}>
-            Submit Form
-          </button> 
-        </div>
-      </form>
+//   return (
+//     <section>
+//       <form action="">
+//         {/* <form action="" onSubmit={handleFormSubmission}> */}
+//         <h2>Typical Form</h2>
+//         <input
+//           type="text"
+//           name="example"
+//           id=""
+//           style={{ margin: "1rem 0" }}
+//           onChange={handleFormInput}
+//         />
+//         <div>
+//           <button type="submit" onClick={handleFormSubmission}>
+//             Submit Form
+//           </button>
+//         </div>
+//       </form>
 
-      <button onClick={handleButtonClick}>Click Me</button>
-    </section>
-  );
-}
+//       <button onClick={handleButtonClick}>Click Me</button>
+//     </section>
+//   );
+// }
 
 function Book(props) {
-  const { img, title, author } = props.book;
+  const { id, img, title, author } = props.book;
+
+  const displayTitle = props.displayValue;
+  const getBooks = props.getBook;
+
+  //or
+
+  //  const {img, title, author, displayValue} = props
+
+  // const displayTitle = function() {
+  //   console.log(title);
+  // }
+
+  const getSingleBook = function() { 
+    getBooks(id);
+  }
+
   return (
     <article className="book">
       <img src={img} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {/* <button onClick={displayTitle}>Display Title</button> */}
+      <button onClick={getSingleBook}>Display Titles</button>
     </article>
   );
 }
